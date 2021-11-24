a6356cb2c13b2d0946d039e0000b38d70a427fa2
Beau Hale <halebeau3@gmail.com>
2021-11-24 13:25:34 -0800

    Finished: Menu.js

--- front-end/src/layout/Menu.js
+++ front-end/src/layout/Menu.js
@@ -1,50 +1,77 @@
 import React from "react";
-
 import { Link } from "react-router-dom";
 
 /**
  * Defines the menu for this application.
- *
  * @returns {JSX.Element}
  */
 
 function Menu() {
   return (
-    <nav className="navbar navbar-dark align-items-start p-0">
-      <div className="container-fluid d-flex flex-column p-0">
-        <Link
-          className="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0"
-          to="/"
-        >
-          <div className="sidebar-brand-text mx-3">
-            <span>Periodic Tables</span>
-          </div>
-        </Link>
-        <hr className="sidebar-divider my-0" />
-        <ul className="nav navbar-nav text-light" id="accordionSidebar">
-          <li className="nav-item">
-            <Link className="nav-link" to="/dashboard">
-              <span className="oi oi-dashboard" />
-              &nbsp;Dashboard
-            </Link>
+    <div>
+      <nav
+        className="nav navbar-nav mt-3 bg-white"
+        style={{ position: "fixed", zIndex: "2" }}
+      >
+        <ul className="nav navbar-nav">
+          <li className="nav-item pl-1">
+            <button
+              type="button"
+              className="btn"
+              data-toggle="tooltip"
+              data-placement="bottom"
+              title="Home"
+            >
+              <Link className="nav-link " to="/">
+                <img
+                  style={{ color: "black" }}
+                  src="https://img.icons8.com/material-rounded/24/000000/home.png"
+                />
+              </Link>
+            </button>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/search">
-              <span className="oi oi-magnifying-glass" />
-              &nbsp;Search
-            </Link>
+
+          <li className="nav-item pl-1">
+            <button
+              type="button"
+              className="btn"
+              data-toggle="tooltip"
+              data-placement="bottom"
+              title="Search"
+            >
+              <Link className="nav-link " to="/search">
+                <img src="https://img.icons8.com/material-outlined/24/000000/search--v1.png" />
+              </Link>
+            </button>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/reservations/new">
-              <span className="oi oi-plus" />
-              &nbsp;New Reservation
-            </Link>
+          <li className="nav-item pl-1">
+            <button
+              type="button"
+              className="btn"
+              data-toggle="tooltip"
+              data-placement="bottom"
+              title="New Reservation"
+            >
+              <Link className="nav-link " to="/reservations/new">
+                <img
+                  className=""
+                  src="https://img.icons8.com/ios-filled/24/000000/reservation.png"
+                />
+              </Link>
+            </button>
           </li>
-          <li className="nav-item">
-            <Link className="nav-link" to="/tables/new">
-              <span className="oi oi-layers" />
-              &nbsp;New Table
-            </Link>
+          <li className="nav-item pl-1">
+            <button
+              type="button"
+              className="btn"
+              data-toggle="tooltip"
+              data-placement="bottom"
+              title="New Table"
+            >
+              <Link className="nav-link " to="/tables/new">
+                <img src="https://img.icons8.com/material-outlined/24/000000/table.png" />
+              </Link>
+            </button>
           </li>
         </ul>
         <div className="text-center d-none d-md-inline">
@@ -54,8 +81,8 @@ function Menu() {
             type="button"
           />
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 }
 
